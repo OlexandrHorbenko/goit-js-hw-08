6a95f11272a2c204cf778a3605c1075bd8b375c2
Olexandr Horbenko <horbenko.o.a@gmail.com>
2023-03-22 23:12:37 +0200

    Do task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const localStorageKey = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+const storedTime = localStorage.getItem(localStorageKey);
+
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+
+function saveCurrentTimeToLocalStorage(data) {
+  localStorage.setItem(localStorageKey, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(saveCurrentTimeToLocalStorage, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+const throttle = require('lodash.throttle');
 
+function saveToLocalStorage(refs) {
+const state = {
+email: refs.emailInputEl.value,
+message: refs.messageInputEl.value,
+};
+localStorage.setItem(refs.localStorageKey, JSON.stringify(state));
+}
+
+const throttledSaveToLocalStorage = throttle(refs => {
+saveToLocalStorage(refs);
+}, 500);
+
+function loadFromLocalStorage(refs) {
+const stateString = localStorage.getItem(refs.localStorageKey);
+if (!stateString) return;
+const { email, message } = JSON.parse(stateString);
+refs.emailInputEl.value = email;
+refs.messageInputEl.value = message;
+}
+
+function clearForm(refs) {
+refs.emailInputEl.value = '';
+refs.messageInputEl.value = '';
+}
+
+const form = document.querySelector('.feedback-form');
+
+const formRefs = {
+emailInputEl: form.querySelector('[name="email"]'),
+messageInputEl: form.querySelector('[name="message"]'),
+localStorageKey: 'feedback-form-state',
+};
+
+form.addEventListener('submit', function handleFormSubmit(evt) {
+evt.preventDefault();
+
+const state = {
+email: formRefs.emailInputEl.value,
+message: formRefs.messageInputEl.value,
+};
+
+console.log(state);
+
+clearForm(formRefs);
+localStorage.removeItem(formRefs.localStorageKey);
+});
+
+loadFromLocalStorage(formRefs);
+
+formRefs.emailInputEl.addEventListener('input', function handleEmailInput() {
+throttledSaveToLocalStorage(formRefs);
+});
+
+formRefs.messageInputEl.addEventListener('input', function handleMessageInput() {
+throttledSaveToLocalStorage(formRefs);
+});
